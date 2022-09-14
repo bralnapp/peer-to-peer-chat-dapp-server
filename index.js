464023689ee0bc7e3d75750faf96e885cd39ef73
coolpythoncodes <coolpythoncodes@gmail.com>
2022-09-14 15:51:07 +0100

    switching to web sockets

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
+const server = require("http").createServer();
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+const PORT = 4000;
+const NEW_CHAT_MESSAGE_EVENT = "newChatMessage";
+
+io.on("connection", (socket) => {
+  
+  // Join a conversation
+  const { roomId } = socket.handshake.query;
+  socket.join(roomId);
+
+  // Listen for new messages
+  socket.on(NEW_CHAT_MESSAGE_EVENT, (data) => {
+    io.in(roomId).emit(NEW_CHAT_MESSAGE_EVENT, data);
+  });
+
+  // Leave the room if the user closes the socket
+  socket.on("disconnect", () => {
+    socket.leave(roomId);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
+
 // in the index.js file
 
-const express = require('express')
-const app = express()
-const port = 5050
-const Gun = require('gun')
+// const express = require('express')
+// const app = express()
+// const port = 5050
+// const Gun = require('gun')
 
-app.use(Gun.serve)
+// app.use(Gun.serve)
 
-const server = app.listen(port, () => {
-  console.log(`Gun server running on port ${port}🔥`)
-})
+// const server = app.listen(port, () => {
+//   console.log(`Gun server running on port ${port}🔥`)
+// })
 
-Gun({ web: server })
+// Gun({ web: server })
